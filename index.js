84556a3cbef8d01f10b0cea183517a963452146f
jaytayade <s-jtayade@lwsd.org>
2014-03-15 16:15:59 -0700

    Set up node script, including package.json for dependencies

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * Module Dependencies
+*/
+var express = require('express');
+var pub = __dirname + "/public";
+//setup middleware
+var app = express();
+app.use(express.static(pub));
+app.set('views', __dirname + "/views");
+//set default template engine to jade
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res, next) {
+	res.render("bootstrap");
+});
+
+app.use(function(err, req, res, next) {
+	res.send(err.stack);
+});
+
+app.listen(3000);
+console.log("Listening on port 3000");
